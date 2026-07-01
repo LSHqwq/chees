8f7b2a43d354f8b4c8ca1ddceaaa4b5a14e150ff
LSHligihtt <3238226563@qq.com>
2026-07-01 13:57:46 +0800

    修改域名

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ const CELL_SIZE = 36;
 const PADDING = 30;
 
 // 本地开发用 localhost，部署到 Railway 后改成你的域名
-const API_URL = 'http://localhost:8000';
+const API_URL = 'chees-backend.up.railway.app';
 
 // 如果部署到 Railway，改成：
 // const API_URL = 'https://你的项目名.up.railway.app';
